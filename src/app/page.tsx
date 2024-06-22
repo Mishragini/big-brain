38b54c2f877a5e9cd5e85f0adeee5e32ed2bd508
Mishragini <mishragini8@gmail.com>
2024-06-22 10:10:30 +0530

    adds shadcn and created a header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,26 +2,15 @@
 import { SignInButton, UserButton } from "@clerk/nextjs";
 import { Authenticated, Unauthenticated, useMutation, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/mode-toggle";
 
 export default function Home() {
   const documents = useQuery(api.documents.getDocuments)
   const createDocument = useMutation(api.documents.createDocument)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-       <Unauthenticated>
-        <SignInButton />
-      </Unauthenticated>
-      <Authenticated>
-        <UserButton />
-        <button onClick={()=>{
-          createDocument({title:'Hello World'})
-        }}>Click Me</button>
-        {
-          documents?.map((doc)=>(
-            <div key={doc._id}>{doc.title}</div>
-          ))
-        }
-      </Authenticated>
+      
     </main>
   );
 }
